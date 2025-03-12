35c1ea7da995bf3d010464a699be4da51e0b55b2
Wasieq <viraelwach@gmail.com>
2025-03-12 08:34:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+funtion skillsMember() {
+    var skills = [
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Node',
+        'Express',
+        'MongoDB',
+        'Git',
+        'GitHub',
+        'Photoshop',
+        'Illustrator',
+        'Figma',
+        'Sketch'
+    ];
+    return skills;
+}
